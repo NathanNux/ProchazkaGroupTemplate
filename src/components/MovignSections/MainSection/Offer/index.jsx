a13b8da644679b81bb496dac975c5d99197c7c94
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-12-07 22:28:35 +0100

    looking for the error 15

--- src/components/MovignSections/MainSection/Offer/index.jsx
+++ src/components/MovignSections/MainSection/Offer/index.jsx
@@ -13,8 +13,8 @@ export default function Offer() {
             <IntroOffer />
             <Reality />
             <BecomeAclient />
-            {/* <Benefits />
-            <TheWay />
+            <Benefits />
+            {/* <TheWay />
             <Requirements />
             <Testimonials />
             <OurDeal /> */}
